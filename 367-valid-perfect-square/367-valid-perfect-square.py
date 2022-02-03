3deb098ca83e2e17a0ee880bab99b75c33f15e9b
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-02-04 00:09:23 +0530

    Time: 32 ms (76.83%), Space: 13.8 MB (96.78%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if sqrt(num) == int(sqrt(num)):
+            return True
+        else:
+            False

0ac1cb2735cdeb50885292026c9ec9628f3ab8b1
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-02-08 20:28:12 +0530

    Time: 36 ms (68.31%), Space: 13.9 MB (81.53%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,12 +1,15 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        sol = 100
-        while sol >= 10:
-            s = str(num)
-            ls = []
-            for i in range(len(s)):
-                ls.append(s[i])
-            ls = list(map(int, ls))
-            sol = sum(ls)
-            num = sum(ls)
-        return sol
+        # sol = 100
+        # while sol >= 10:
+        #     s = str(num)
+        #     ls = []
+        #     for i in range(len(s)):
+        #         ls.append(s[i])
+        #     ls = list(map(int, ls))
+        #     sol = sum(ls)
+        #     num = sum(ls)
+        # return sol
+        if num == 0 : return 0
+        if num % 9 == 0 : return 9
+        else : return (num % 9)

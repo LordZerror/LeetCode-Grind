19d756621ce725c4ac6fd9bfeb06b9227d96d87e
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-02-17 19:36:58 +0530

    Time: 34 ms (83.40%), Space: 13.7 MB (99.28%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,18 +1,41 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        if n == 1 or n == 7:
-            return True
-        elif 1 < n <= 9:
-            return False
-        else:
-            while n > 9:
-                n = list(map(int, str(n)))
-                ans = 0
-                for i in n:
-                    ans = ans + i**2
-                n = ans
-            if n == 1 or n == 7:
+        # if n == 1 or n == 7:
+        #     return True
+        # elif 1 < n <= 9:
+        #     return False
+        # else:
+        #     while n > 9:
+        #         n = list(map(int, str(n)))
+        #         ans = 0
+        #         for i in n:
+        #             ans = ans + i**2
+        #         n = ans
+        #     if n == 1 or n == 7:
+        #         return True
+        #     elif 1 < n <= 9:
+        #         return False
+        check = []
+        n = str(n)
+
+        while True:
+            total = 0
+            
+			# Taking the the number and sum of the squares of its digits, storing the result in 'total'
+            for i in n:
+                total += int(i) * int(i)
+                
+			# Checking if 'total' is the happy number
+            if total == 1:
                 return True
-            elif 1 < n <= 9:
+				
+			# If total is already in check list, then it entered a loop
+            if total in check:
                 return False
+            
+			# Appending 'total' to check list
+            check.append(total)
+            
+			# Assigning the new number to 'n', so it passes through the same steps
+            n = str(total)
         

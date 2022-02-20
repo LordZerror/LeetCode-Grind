2034ab3c9c635eebf6167a78a0e43c08b6beef5d
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-02-20 10:26:52 +0530

    Time: 823 ms (58.89%), Space: 21.2 MB (98.09%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,3 +1,29 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        # return sorted(nums)
+        if len(nums) > 1:
+            leftArray = nums[:len(nums)//2]
+            rightArray = nums[len(nums)//2:]
+            self.sortArray(leftArray)
+            self.sortArray(rightArray)
+            i, j, k = 0, 0, 0
+            while i < len(leftArray) and j < len(rightArray):
+                if leftArray[i] < rightArray[j]:
+                    nums[k] = leftArray[i]
+                    i += 1
+                else:
+                    nums[k] = rightArray[j]
+                    j += 1
+                k += 1
+            # When Right Array is Empty
+            while i < len(leftArray):
+                nums[k] = leftArray[i]
+                i += 1
+                k += 1
+            # When Left Array is Empty
+            while j < len(rightArray):
+                nums[k] = rightArray[j]
+                j += 1
+                k += 1
+            return nums
+        return nums

8da825b3691e527aff452f0acda029b4e30f8a17
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-02-21 23:30:41 +0530

    Time: 76 ms (5.54%), Space: 13.9 MB (59.30%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        sol = []
+        ans = str(num)
+        for i in ans:
+            sol.append(int(i))
+        for i in range(len(sol)):
+            if sol[i] == 6:
+                sol[i] = 9
+                break
+        sol = list(map(str, sol))
+        res = int("".join(sol))
+        return res

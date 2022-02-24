a4c349c44d28e03576f8e5d6507ee96a7b2b41a4
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-02-24 09:58:13 +0530

    Added solution - LeetHub

---
+++ Factorial - GFG/factorial.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+
+class Solution:
+    def factorial (self, N):
+        # code here
+        prod = 1
+        for i in range(1, N+1):
+            prod = prod * i
+        return prod
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        ob = Solution()
+        print(ob.factorial(N))
+
+# } Driver Code Ends

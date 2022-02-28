917596fe1388c277730dbc3c4dc4855109b1420f
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-02-28 23:40:22 +0530

    Added solution - LeetHub

---
+++ 5. Modules - GFG/5.-modules.py
@@ -0,0 +1,25 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+from math import *
+
+def math_func():
+    #import math module and print pi and e
+    print("pi =", pi)
+    print("e =", e)
+    
+    
+
+#{ 
+#Driver Code Starts.
+
+def main():
+    math_func()
+    
+    
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends

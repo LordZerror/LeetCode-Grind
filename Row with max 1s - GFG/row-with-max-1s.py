a01eb5f0dd2fe9f9d626dc7e5a0235fd6bce7328
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-03-17 01:49:21 +0530

    Added solution - LeetHub

---
+++ Row with max 1s - GFG/row-with-max-1s.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+class Solution:
+
+	def rowWithMax1s(self,arr, n, m):
+		# code here
+		ans = []
+        for i in arr:
+            ans.append(sum(i))
+        
+        if len(set(ans)) == 1 and 0 in ans:
+            return -1
+        return ans.index(max(ans))
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, m = list(map(int, input().strip().split()))
+        
+        inputLine = list(map(int, input().strip().split()))
+        arr = [[0 for j in range(m)] for i in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                arr[i][j] = inputLine[i * m + j]
+        ob = Solution()
+        ans = ob.rowWithMax1s(arr, n, m)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends

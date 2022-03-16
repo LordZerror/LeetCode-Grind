c60b3b0d5577d0817e4e48f7bc49ad66be59b8cc
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-03-16 23:04:14 +0530

    Time: 72 ms (92.53%), Space: 14.1 MB (63.80%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.py
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -4,7 +4,7 @@ class Solution:
         i=0
         for num in pushed:
             stack.append(num) #we are pushing the number to the stack
-            while  len(stack) >0 and stack[len(stack)-1] == popped[i] :
+            while  len(stack) >0 and stack[-1] == popped[i] :
                 #if the last element of the stack is equal to the popped element
                 stack.pop()
                 i+=1 #we are incrementing i

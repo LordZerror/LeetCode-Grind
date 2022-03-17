5a451279dbf4feee432f672caf2bfe104fd96cf4
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-03-17 19:10:08 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.py
+++ Palindrome String - GFG/palindrome-string.py
@@ -2,10 +2,7 @@
 class Solution:
 	def isPalindrome(self, S):
 		# code here
-		if S == S[::-1]:
-		    return 1
-		else:
-		    return 0
+		return 1 if S == S[::-1] else 0
 
 #{ 
 #  Driver Code Starts

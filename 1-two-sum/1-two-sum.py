ebc0d8d8ceb930ec134d173863a81dd0a5e8995f
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-05-21 00:36:04 +0530

    Time: 1451 ms (30.09%), Space: 15 MB (76.40%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {} # Dictionary to store index and value of the elements of the array
-        for i, value in enumerate(nums): #1 enumerate comes handy in a lot of problems (I mean if you want to have a cleaner code of course)
-            remaining = target - nums[i] #2
-            if remaining in seen: #3
-                return [i, seen[remaining]]  #4
-            else:
-                seen[value] = i  #5
-        
+        for i in range(len(nums)):
+            if target - nums[i] in nums:
+                if i != nums.index(target - nums[i]):
+                    return [i, nums.index(target - nums[i])]

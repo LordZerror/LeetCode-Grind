b84d1d87933525aca3db3537f3382d3cb46e50d6
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-06-03 00:13:18 +0530

    Added solution - LeetHub

--- Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.py
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.py
@@ -1,10 +1,13 @@
 #User function Template for python3
 
 def rotate( ls, n):
-    nums = ls.copy()
-    for i in range(n):
-        ls[i] = nums[(i+n-1)%n]
+    # nums = ls.copy()
+    # for i in range(n):
+    #     ls[i] = nums[(i+n-1)%n]
     
+    ls.insert(0, ls[-1])
+    ls.pop(-1)
+    # return ls
     
 
 

12b90ae31411563b99ce852127d51ac77827130b
Kaushal Patil <89535024+LordZerror@users.noreply.github.com>
2022-06-13 01:04:34 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.py
+++ Reverse a String - GFG/reverse-a-string.py
@@ -2,7 +2,12 @@
 
 def reverseWord(s):
     #your code here
-    return s[::-1]
+    # return s[::-1]
+    s = list(s)
+    for i in range(len(s)//2):
+        s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
+    
+    return ''.join(s)
 
 #{ 
 #  Driver Code Starts
